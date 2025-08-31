7d45b4cfd05d7dde5bffba35c069ea39eeff1552
rajgupta2399 <rajkumardotnet2499@gmail.com>
2025-08-31 22:59:23 +0530

    changes

--- src/app/(admin)/page.jsx
+++ src/app/(admin)/page.jsx
@@ -8,8 +8,8 @@ import React from "react"
 
 export const metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template"
+    "Dashboard",
+  description: "TailAdmin Dashboard"
 }
 
 export default function Ecommerce() {
